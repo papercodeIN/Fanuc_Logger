b45a3b43449f40f99fb42640939545e17de33053
Fusion Automate <eng.innovativ@gmail.com>
2022-04-10 12:31:18 +0530

    Add files via upload

---
+++ Database/example.py
@@ -0,0 +1,18 @@
+import sys
+
+def absolute_value(num):
+	"""This function returns the absolute
+	value of the entered number"""
+
+	if num >= 0:
+		return num
+	else:
+		return -num
+
+# Output: 2
+#print(absolute_value(2))
+
+while True:
+    num = sys.stdin.readline() # read the stdin from the inject node
+    num = int(num)
+    print(absolute_value(num))
